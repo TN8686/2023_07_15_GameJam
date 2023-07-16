e6bd6dffef57e7911eab53ce89c224f71bb5c51b
kentyisapen <kenkenpoco@gmail.com>
2023-07-16 10:35:31 +0900

    delete logging

--- testProject/Assets/Scripts/AntisepticManager.cs
+++ testProject/Assets/Scripts/AntisepticManager.cs
@@ -10,7 +10,6 @@ public class AntisepticManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(AntisepticTime);
     }
 
     // Update is called once per frame
