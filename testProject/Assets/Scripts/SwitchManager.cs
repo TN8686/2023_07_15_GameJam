abf7570ff464b52a86f97908d5f4978b0a410bd5
kentyisapen <kenkenpoco@gmail.com>
2023-07-16 11:18:43 +0900

    add Switch Logic

--- testProject/Assets/Scripts/SwitchManager.cs
+++ testProject/Assets/Scripts/SwitchManager.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class SwitchManager : MonoBehaviour
 {
+    [SerializeField]
+    private bool isPushed = false;
+    private int count = 0;
+    public bool IsPushed { get { return isPushed; } set { isPushed = value; } }
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
+    void onCollisionEnter2D(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Block")
+        {
+            count++;
+        }
+    }
+
+    void onCollisionExit2D(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Block")
+        {
+            count--;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        IsPushed = (count > 0);
     }
 }
