b5dd3649b02051931b132defd4ea3f7abf1c4053
kentyisapen <kenkenpoco@gmail.com>
2023-07-17 12:07:13 +0900

    add: blood

---
+++ testProject/Assets/Scripts/BloodManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodManager : MonoBehaviour
+{
+    [SerializeField]
+    // フェードのスピード 
+    private float fadingSpeed = 0.01f;
+
+    // フェードしているか
+    private bool isFading = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color color = GetComponent<SpriteRenderer>().color;
+        if (isFading)
+        {
+            // 0までフェード
+            if (color.a > 0)
+            {
+                color.a -= fadingSpeed;
+            }
+        }
+        else
+        {
+            // フェードする前、まず透明度を1にする
+            color.a = 1;
+            isFading = true;
+        }
+
+        GetComponent<SpriteRenderer>().color = color;
+    }
+
+
+}
